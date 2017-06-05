4e101aa4ccd7b79927f27624340a5f6176791586
Fyping <18779875304@163.com>
2017-06-05 20:55:03 +0800

    descript file since 2170605

--- utils/createSqlFile.py
+++ utils/createSqlFile.py
@@ -2,10 +2,22 @@ import os
 
 #url ./../sql/mobile.sql
 
+# def createFile(url,content):
+#     count=0
+#     with open(url,'a') as f:
+#         f.write("INSERT INTO mobile_basic(mb_id,mb_desc ,mb_img_addr)VALUES")
+#         for i in content:
+#             if count!=len(content)-1:
+#                 f.write(i+",")
+#             else:
+#                 f.write(i)
+#             count=count+1
+#         f.write(";\r\n\r\n")
+
 def createFile(url,content):
     count=0
     with open(url,'a') as f:
-        f.write("INSERT INTO mobile_basic(mb_id,mb_desc ,mb_img_addr)VALUES")
+        f.write("INSERT INTO tb_GoodsInfo(goodsid,goodsbrand,goodsdescript ,goodsurl)VALUES")
         for i in content:
             if count!=len(content)-1:
                 f.write(i+",")
@@ -13,5 +25,4 @@ def createFile(url,content):
                 f.write(i)
             count=count+1
         f.write(";\r\n\r\n")
-
 
